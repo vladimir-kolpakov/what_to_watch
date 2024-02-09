46fa749e229fab81faafe8edcae311ef241f3f67
Владимир Колпаков <vix-sp@yandex.ru>
2024-02-09 17:45:04 +0300

    Save

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 # what_to_watch/settings.py
 
+import os
+
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'MY_SECRET_KEY'
+    SECRET_KEY = os.getenv('SECRET_KEY')

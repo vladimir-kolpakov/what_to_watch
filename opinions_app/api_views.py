1b397771da9ab3058f63b68381454d13fe24bdac
Владимир Колпаков <vix-sp@yandex.ru>
2024-02-09 17:53:48 +0300

    Save

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,14 +1,14 @@
 # what_to_watch/opinions_app/api_views.py
 
 # Импортируем метод jsonify
-from flask import jsonify  
+from flask import jsonify
 
 from . import app
 from .models import Opinion
 
 
 # Явно разрешить метод GET
-@app.route('/api/opinions/<int:id>/', methods=['GET'])  
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     # Получить объект по id или выбросить ошибку
     opinion = Opinion.query.get_or_404(id)

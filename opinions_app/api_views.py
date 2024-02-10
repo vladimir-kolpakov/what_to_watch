28e5f8fd8f6b6d161fa2ab131dd21f034e693ed3
Владимир Колпаков <vix-sp@yandex.ru>
2024-02-10 21:36:58 +0300

    Save

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,10 +1,20 @@
 # what_to_watch/opinions_app/api_views.py
 
 # Импортируем метод jsonify
-from flask import jsonify
+from flask import jsonify, request
 
-from . import app
+from . import app, db
 from .models import Opinion
+
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    # Запрашивается список объектов
+    opinions = Opinion.query.all()
+    # Поочерёдно сериализуется каждый объект,
+    # а потом все объекты помещаются в список opinions_list
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify({'opinions': opinions_list}), 200
 
 
 # Явно разрешить метод GET
@@ -14,3 +24,28 @@ def get_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     # Конвертировать данные в JSON и вернуть объект и код ответа API
     return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    opinion = Opinion.query.get_or_404(id)
+    # Если метод get_or_404 не найдёт указанный ключ,
+    # то он выбросит исключение 404
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    # Все изменения нужно сохранить в базе данных
+    db.session.commit()
+    # При создании или изменении объекта вернём сам объект и код 201
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/opinions/<int:id>/', methods=['DELETE'])
+def delete_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    # При удалении принято возвращать только код ответа 204
+    return '', 204

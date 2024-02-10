5bfe40679ce6d041397e1cf3be41e17d8dc19b37
Владимир Колпаков <vix-sp@yandex.ru>
2024-02-10 22:13:54 +0300

    Save

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -5,38 +5,19 @@ from flask import jsonify, request
 
 from . import app, db
 from .models import Opinion
-
-
-@app.route('/api/opinions/', methods=['POST'])
-def add_opinion():
-    # Получение данные из запроса в виде словаря
-    data = request.get_json()
-    # Создание нового пустого экземпляра модели
-    opinion = Opinion()
-    # Наполнение его данными из запроса
-    opinion.from_dict(data)
-    # Добавление новой записи в базу данных
-    db.session.add(opinion)
-    # Сохранение изменений
-    db.session.commit()
-    return jsonify({'opinion': opinion.to_dict()}), 201
-
-
-@app.route('/api/opinions/', methods=['GET'])
-def get_opinions():
-    # Запрашивается список объектов
-    opinions = Opinion.query.all()
-    # Поочерёдно сериализуется каждый объект,
-    # а потом все объекты помещаются в список opinions_list
-    opinions_list = [opinion.to_dict() for opinion in opinions]
-    return jsonify({'opinions': opinions_list}), 200
+from .views import random_opinion
+# Импорт исключения
+from .error_handlers import InvalidAPIUsage
 
 
 # Явно разрешить метод GET
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     # Получить объект по id или выбросить ошибку
-    opinion = Opinion.query.get_or_404(id)
+    opinion = Opinion.query.get(id)
+    if opinion is None:
+        # Тут код ответа нужно указать явным образом
+        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
     # Конвертировать данные в JSON и вернуть объект и код ответа API
     return jsonify({'opinion': opinion.to_dict()}), 200
 
@@ -44,6 +25,14 @@ def get_opinion(id):
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
     data = request.get_json()
+    if 'title' not in data or 'text' not in data:
+        # Выбрасываем собственное исключение.
+        # Второй параметр (статус-код) можно не передавать:
+        # нужно вернуть код 400, а именно он возвращается по умолчанию
+        raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        # Выбрасываем собственное исключение
+        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
     opinion = Opinion.query.get_or_404(id)
     # Если метод get_or_404 не найдёт указанный ключ,
     # то он выбросит исключение 404
@@ -59,8 +48,54 @@ def update_opinion(id):
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
-    opinion = Opinion.query.get_or_404(id)
+    opinion = Opinion.query.get(id)
+    if opinion is None:
+        # Тут код ответа нужно указать явным образом
+        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
     db.session.delete(opinion)
     db.session.commit()
     # При удалении принято возвращать только код ответа 204
     return '', 204
+
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    # Запрашивается список объектов
+    opinions = Opinion.query.all()
+    # Поочерёдно сериализуется каждый объект,
+    # а потом все объекты помещаются в список opinions_list
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify({'opinions': opinions_list}), 200
+
+
+@app.route('/api/opinions/', methods=['POST'])
+def add_opinion():
+    # Получение данные из запроса в виде словаря
+    data = request.get_json()
+    # Создание нового пустого экземпляра модели
+    # Если нужных ключей нет в словаре,
+    if 'title' not in data or 'text' not in data:
+        # Выбрасываем собственное исключение.
+        # Второй параметр (статус-код) можно не передавать:
+        # нужно вернуть код 400, а именно он возвращается по умолчанию
+        raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        # Выбрасываем собственное исключение
+        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
+    opinion = Opinion()
+    # Наполнение его данными из запроса
+    opinion.from_dict(data)
+    # Добавление новой записи в базу данных
+    db.session.add(opinion)
+    # Сохранение изменений
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    if opinion is not None:
+        return jsonify({'opinion': opinion.to_dict()}), 200
+    # Тут код ответа нужно указать явным образом
+    raise InvalidAPIUsage('В базе данных нет мнений', 404)
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -7,14 +7,20 @@ from .forms import OpinionForm
 from .models import Opinion
 
 
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+
 @app.route('/')
 def index_view():
-    quantity = Opinion.query.count()
-    if not quantity:
-        abort(404)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
-    return render_template('opinion.html', opinion=opinion)
+    opinion = random_opinion()
+    if opinion is not None:
+        return render_template('opinion.html', opinion=opinion)
+    abort(404)
 
 
 @app.route('/add', methods=['GET', 'POST'])
